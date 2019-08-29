dd9059623d44e4996b65c7f96603c72b19493b0f
moooofly <centos.sf@gmail.com>
2019-08-29 16:29:05 +0800

    feat: integrate grpc-role-service call into redis probe

--- probe/redis/redis.go
+++ probe/redis/redis.go
@@ -1,12 +1,17 @@
 package redis
 
 import (
+	"context"
 	"errors"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/moooofly/dms-detector/pkg/parser"
 	"github.com/moooofly/dms-detector/probe"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	pb "github.com/moooofly/dms-detector/proto"
 )
 
 type RedisProbeArgs struct {
@@ -41,17 +46,11 @@ func (s *RedisProbe) StopProbe() {
 	s.isStop = true
 }
 
-func (s *RedisProbe) Start(args interface{}, log *logrus.Logger) (err error) {
+func (s *RedisProbe) Start(args interface{}, log *logrus.Logger) error {
 	s.log = log
 	//s.cfg = args.(RedisProbeArgs)
 
-	if ok := s.detect(); ok {
-		return nil
-	} else {
-		return errors.New("detect failed")
-	}
-
-	return
+	return s.detect()
 }
 
 func (s *RedisProbe) Clean() {
@@ -71,14 +70,16 @@ func (s *RedisProbe) Clean() {
 //     3.1.2 如果连接建立成功，则判定所连接的 elector 的 role ，如果是 leader 则认为 detect 成功，否则认为 detect 失败
 //   3.2 如果 strict 为 off ，则直接判定 detect 失败
 // 4. 如果 role 为 slave ，则直接判定 detect 失败
-func (s *RedisProbe) detect() bool {
+func (s *RedisProbe) detect() error {
 	s.log.Println("[detector/redis] --> probe start")
 	defer s.log.Println("[detector/redis] <-- probe done")
+
+	s.log.Println("[detector/redis]   --> try to connect Redis")
 
 	c, err := redis.Dial("tcp", parser.RedisSetting.Target)
 	if err != nil {
 		s.log.Println("[detector/redis]", err)
-		return false
+		return err
 	} else {
 		s.log.Printf("[detector/redis] connect Redis[%s] success\n", parser.RedisSetting.Target)
 	}
@@ -87,36 +88,54 @@ func (s *RedisProbe) detect() bool {
 		if _, err := c.Do("AUTH", parser.RedisSetting.Password); err != nil {
 			c.Close()
 			s.log.Println("[detector/redis]", err)
-			return false
+			return err
 		}
 	}
 	defer c.Close()
 
 	if isMaster(c) {
-		s.log.Println("[detector/redis] redis role -> [master]")
+		s.log.Println("[detector/redis] redis role => [master]")
+
 		if parser.RedisSetting.Strict == "ON" {
-			s.log.Println("[detector/redis] try to connect elector")
-			if true {
-				s.log.Printf("[detector/redis] connect elector[%s] success", parser.DetectorSetting.ElectorHost)
-				if true {
-					s.log.Println("[detector/redis] elector role -> [leader]")
-					return true
-				} else {
-					s.log.Println("[detector/redis] elector role -> [follower]")
-					return false
-				}
+			s.log.Println("[detector/redis]     --> try to connect elector (Strict=ON)")
+
+			// TODO: 连接复用问题
+			conn, err := grpc.Dial(
+				parser.DetectorSetting.ElectorHost,
+				grpc.WithInsecure(),
+				grpc.WithBlock(),
+				grpc.WithTimeout(time.Second),
+			)
+			if err != nil {
+				s.log.Printf("[detector/redis] connect elector[%s] failed, err => [%v]", parser.DetectorSetting.ElectorHost, err)
+				return errors.New("connect elector failed")
+			}
+			defer conn.Close()
+
+			s.log.Printf("[detector/redis] connect elector[%s] success", parser.DetectorSetting.ElectorHost)
+
+			client := pb.NewRoleServiceClient(conn)
+			obRsp, err := client.Obtain(context.Background(), &pb.ObtainReq{})
+
+			if err != nil {
+				s.log.Infof("[detector/redis] Obtain role failed: %v", err)
+				return errors.New("obtain role from elector failed")
+			}
+
+			s.log.Infof("[detector/redis] role => [%s]", obRsp.GetRole())
+
+			if pb.EnumRole_Leader == obRsp.GetRole() {
+				return nil
 			} else {
-				s.log.Printf("[detector/redis] connect elector[%s] failed", parser.DetectorSetting.ElectorHost)
-				return false
+				return errors.New("role of elector is not Leader")
 			}
 		} else {
-			return false
+			return errors.New("redis role == master && strict == OFF")
 		}
 	} else {
-		s.log.Println("[detector/redis] redis role -> [non-master]")
+		s.log.Println("[detector/redis] redis role => [slave], can not be detected")
+		return errors.New("redis role == slave")
 	}
-
-	return true
 }
 
 func isMaster(conn redis.Conn) bool {
